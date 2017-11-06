e0d4f2937c24a4ab97322d6b8f6f8132ace12e96
Slobodanka Mihajlov <sekanececa@hotmail.com>
2017-11-06 23:09:42 +0100

    Project 2 till step7

---
+++ AngularJSAuthebtication/AuthRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using AngularJSAuthebtication.Models;
+using System.Threading.Tasks;
+
+
+namespace AngularJSAuthebtication
+{
+    public class AuthRepository : IDisposable
+    {
+
+        //we are depending on the “UserManager” that provides the domain logic
+        //for working with user information. The “UserManager” knows when to hash 
+        //a password, how and when to validate a user, and how to manage claims. 
+        private AuthContext _ctx;
+
+        private UserManager<IdentityUser> _userManager;
+
+        public AuthRepository()
+        {
+            _ctx = new AuthContext();
+            _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserModel userModel)
+        {
+            IdentityUser user = new IdentityUser
+            {
+                UserName = userModel.UserName
+            };
+            var result = await _userManager.CreateAsync(user, userModel.Password);
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            IdentityUser user = await _userManager.FindAsync(userName, password);
+            return user;
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+            _userManager.Dispose();
+        }
+    }
+}
---
+++ AngularJSAuthebtication/Controllers/Account2Controller.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using AngularJSAuthebtication.Models;
+using Microsoft.AspNet.Identity;
+using System.Web.Http.Owin;
+
+
+namespace AngularJSAuthebtication.Controllers
+{
+    [RoutePrefix("api/Account2")]
+    public class Account2Controller : ApiController
+    {
+        private AuthRepository _repo = null;
+        public Account2Controller()
+        {
+            _repo = new AuthRepository();
+        }
+
+        //    //POST api/Account/Register
+        //    [AllowAnonymous]
+        //    [Route("Register")]
+        //   // [AcceptVerbs("POST")]
+        //    public async Task<IHttpActionResult> Register(UserModel userModel)
+        //    {
+        //        if (!ModelState.IsValid)
+        //        {
+        //            return BadRequest(ModelState);
+        //        }
+
+        //        IdentityResult result = await _repo.RegisterUser(userModel);
+
+        //        IHttpActionResult errorResult = GetErrorResult(result);
+        //        if (errorResult !=null)
+        //        {
+        //            return errorResult;
+        //        }
+        //        return Ok();
+        //    }
+        //    protected override void Dispose(bool disposing)
+        //    {
+        //        if (disposing)
+        //        {
+        //            _repo.Dispose();
+        //        }
+        //        base.Dispose(disposing);
+        //    }
+        //    private IHttpActionResult GetErrorResult(IdentityResult result)
+        //    {
+        //        if (result == null)
+        //        {
+        //            return InternalServerError();
+        //        }
+        //        if (!result.Succeeded)
+        //        {
+        //            if (result.Errors != null)
+        //            {
+
+        //                foreach (string error in result.Errors)
+        //                {
+        //                    ModelState.AddModelError("", error);
+        //                }
+        //            }
+
+        //            if (ModelState.IsValid)
+        //            {
+        //                // No ModelState errors are available to send, so just return an empty BadRequest.
+        //                return BadRequest();
+        //            }
+        //            return BadRequest(ModelState);
+
+        //        }
+        //        return null;
+        //    }
+        // POST api/Account/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await _repo.RegisterUser(userModel);
+
+            IHttpActionResult errorResult = GetErrorResult(result);
+
+            if (errorResult != null)
+            {
+                return errorResult;
+            }
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+    }
+
+}
