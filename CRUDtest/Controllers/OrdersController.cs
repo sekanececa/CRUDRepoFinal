d5e903aae813b5d256f818961fc0ccc085d07416
Slobodanka Mihajlov <sekanececa@hotmail.com>
2017-11-09 16:59:38 +0100

    Deleted folders and some changes on existing project

---
+++ CRUDtest/Controllers/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRUDtest.Controllers
+{
+    [RoutePrefix("api/Orders")]
+    public class OrdersController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(Order.CreateOrder());
+        }
+
+        public class Order
+        {
+            public int OrderId { get; set; }
+            public string CustomerName { get; set; }
+            public string ShipperCity { get; set; }
+            public Boolean IsShipped { get; set; }
+
+            public static List<Order> CreateOrder()
+            {
+                List<Order> OrderList = new List<Order>()
+                {
+                    new Order { OrderId=1111,CustomerName="Ivan Marjanovic",ShipperCity="BG", IsShipped=true},
+                    new Order { OrderId=2222,CustomerName="Milica Markovic",ShipperCity="TW", IsShipped=false},
+                    new Order { OrderId=3333,CustomerName="Milica Bogdanovic",ShipperCity="VF", IsShipped=false},
+                    new Order { OrderId=4444,CustomerName="Miroslav Mrdakovic",ShipperCity="NS", IsShipped=true}
+                };
+                return OrderList;
+            }
+
+        }
+    }
+}
