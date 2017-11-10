7b75b6af9c8be85dbd073a9ee11e309d06529a51
Slobodanka Mihajlov <sekanececa@hotmail.com>
2017-11-10 11:48:14 +0100

    AspNetRoles

--- CRUDtest/Startup.cs
+++ CRUDtest/Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.Owin;
 using Owin;
 using Microsoft.Owin.Security.OAuth;
 using CRUDtest.Providers;
+using CRUDtest.Models;
+using Microsoft.AspNet.Identity;// za RoleManager
+using Microsoft.AspNet.Identity.EntityFramework;
 
 [assembly: OwinStartup(typeof(CRUDtest.Startup))]
 
@@ -15,6 +18,7 @@ namespace CRUDtest
         public void Configuration(IAppBuilder app)
         {
             ConfigureOAuth(app);
+            createRolesandUsers();//10.11.2017.
         }
 
         public void ConfigureOAuth(IAppBuilder app)
@@ -31,6 +35,62 @@ namespace CRUDtest
             app.UseOAuthAuthorizationServer(OAuthServerOptions);
             app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
 
+        }
+        //10.11.2017
+        //pravi tabelu u SQL AspNetRoles koja daje id za odredjenu ulogu usera u app-u
+
+        // In this method we will create default User roles and Admin user for login   
+        public void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "shanu";
+                user.Email = "syedshanumcain@gmail.com";
+
+                string userPWD = "A@Z200711";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role    
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Employee role    
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+
+            }
         }
     }
 }
